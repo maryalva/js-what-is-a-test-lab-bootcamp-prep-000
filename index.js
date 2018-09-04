62d2b253d62fc35fa384c28616d232a660247f78
Mary A <mary.alva@icloud.com>
2018-09-04 16:15:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,4 +4,5 @@ var height = 74
 var message = `${name} is ${height} inches tall`
 var name ="Susan"
 var height = 39
+var message = `${name} is ${height} inches tall`
 module.exports = {name, height, message}
